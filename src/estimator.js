c7d66c4e5f3fc5e048137c4b82141af5c7dbfd94
Akande David <adavidoladele@gmail.com>
2020-04-11 17:49:08 +0100

    prettifier

--- src/estimator.js
+++ src/estimator.js
@@ -5,36 +5,34 @@ const covid19ImpactEstimator = (data) => {
   impact.currentlyInfected = data.reportedCases * 10;
   severeImpact.currentlyInfected = data.reportedCases * 50;
 
-  converttodays = (data) => {
-    if (data.periodType === 'days') {
-      return parseInt(data.timeToElapse / 3);
+  let converttodays = (data) => {
+      let date= null
+    if (data.periodType === 'days') { 
+      date= parseInt(data.timeToElapse / 3);
+      return date
     } else if (data.periodType === 'week') {
-      return parseInt((data.timeToElapse * 7) / 3);
+      date= parseInt((data.timeToElapse * 7) / 3);
+      return date
     } else if (data.periodType === 'month') {
-      return parseInt((data.timeToElapse * 30) / 3);
+      date= parseInt((data.timeToElapse * 30) / 3);
+      return date
     }
+    return date
   };
 
-  impact.infectionsByRequestedTime =
-    impact.currentlyInfected * Math.pow(2, converttodays(data));
-  severeImpact.infectionsByRequestedTime =
-    severeImpact.currentlyInfected * Math.pow(2, converttodays(data));
-  severeImpact.severeCasesByRequestedTime =
-    severeImpact.infectionsByRequestedTime * 0.15;
+  impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** converttodays(data));
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** converttodays(data));
+  severeImpact.severeCasesByRequestedTime = severeImpact.infectionsByRequestedTime * 0.15;
   impact.severeCasesByRequestedTime = impact.infectionsByRequestedTime * 0.15;
 
-  impact.hospitalBedsByRequestedTime =
-    data.totalHospitalBeds * 0.35 - impact.severeCasesByRequestedTime;
-  severeImpact.hospitalBedsByRequestedTime =
-    data.totalHospitalBeds * 0.35 - severeImpact.severeCasesByRequestedTime;
+  impact.hospitalBedsByRequestedTime = data.totalHospitalBeds * 0.35 - impact.severeCasesByRequestedTime;
+  severeImpact.hospitalBedsByRequestedTime = data.totalHospitalBeds * 0.35 - severeImpact.severeCasesByRequestedTime;
 
   impact.casesForICUByRequestedTime = impact.infectionsByRequestedTime * 0.05;
-  severeImpact.casesForICUByRequestedTime =
-    severeImpact.infectionsByRequestedTime * 0.05;
+  severeImpact.casesForICUByRequestedTime = severeImpact.infectionsByRequestedTime * 0.05;
 
   impact.infectionsByRequestedTime = impact.infectionsByRequestedTime * 0.2;
-  severeImpact.infectionsByRequestedTime =
-    severeImpact.infectionsByRequestedTime * 0.2;
+  severeImpact.infectionsByRequestedTime = severeImpact.infectionsByRequestedTime * 0.2;
 
   return {
     impact,
