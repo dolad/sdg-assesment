7d99a28a06e347afef515b56f7f6d51d985e4fc3
Akande David <adavidoladele@gmail.com>
2020-04-11 16:56:24 +0100

    debugging

--- src/estimator.js
+++ src/estimator.js
@@ -1,50 +1,6 @@
 
 const covid19ImpactEstimator = (data) => {
-     let impact={};
-     let severeImpact={};
-    
-    impact={
-        currentlyInfected:(data) => {
-            return data.reportedCases*10
-        },
-        infectionsByRequestedTime:(currentlyInfected, data) => {
-            if (data.periodType === "days")
-            {
-                time=data.timeToElapse / 3;
-                return currentlyInfected * Math.pow(2, time)
-            }else if (data.periodType === "week")
-            {
-                time= parseInt((data.timeToElapse * 7) / 3)
-                return currentlyInfected * Math.pow(2,time)
-            }
-            else if (data.periodType === "month"){
-                time=parseInt((data.timeToElapse * 30) / 3)
-                return currentlyInfected * Math.pow(2,time)
-            }
-        }
-    };
-    severeImpact={
-        currentlyInfected:(data) => {
-            return data.reportedCases*50
-        },
-        infectionsByRequestedTime:(currentlyInfected,data) => {
-            if(data.periodType === "days")
-            {
-                time=data.timeToElapse / 3;
-                return currentlyInfected * Math.pow(2,time)
-            }else if(data.periodType === "week")
-            {
-                time= parseInt((data.timeToElapse * 7) / 3)
-                return currentlyInfected * Math.pow(2,time)
-            }
-            else if(data.periodType === "month"){
-                time=parseInt((data.timeToElapse * 30)/ 3)
-                return currentlyInfected * Math.pow(2,time)
-            }
-        },
-        
-    
-    }
+   data
 
 };
 
