90ce349d3862a1ebc0244c3fc22d0dd7b3738e2f
Akande David <adavidoladele@gmail.com>
2020-04-11 16:16:19 +0100

    refactor

--- src/estimator.js
+++ src/estimator.js
@@ -18,35 +18,99 @@ let data = {
 //  const severe={};
 
 const covid19ImpactEstimator = (data) => {
+     this.data=data
     // let currentlyInfected;
-    let impact={};
-    let severeImpact={};
+    impact={
+        currentlyInfected:(data)=>{
+            console.log(data.reportedCases*10)
+            return data.reportedCases*10
+        },
+        infectionsByRequestedTime:(currentlyInfected,data)=>{
+            if(data.periodType=="days")
+            {
+                time=data.timeToElapse / 3;
+                return currentlyInfected * Math.pow(2,time)
+            }else if(data.periodType=="week")
+            {
+                time= parseInt((data.timeToElapse * 7) / 3)
+                return currentlyInfected * Math.pow(2,time)
+            }
+            else if(data.periodType=="month"){
+                time=parseInt((data.timeToElapse * 30)/ 3)
+                return currentlyInfected * Math.pow(2,time)
+            }
+        }
+    };
+    severeImpact={
+        currentlyInfected:(data)=>{
+            console.log(data.reportedCases*50)
+            return data.reportedCases*50
+        },
+        infectionsByRequestedTime:(currentlyInfected,data)=>{
+            if(data.periodType=="days")
+            {
+                time=data.timeToElapse / 3;
+                return currentlyInfected * Math.pow(2,time)
+            }else if(data.periodType=="week")
+            {
+                time= parseInt((data.timeToElapse * 7) / 3)
+                return currentlyInfected * Math.pow(2,time)
+            }
+            else if(data.periodType=="month"){
+                time=parseInt((data.timeToElapse * 30)/ 3)
+                return currentlyInfected * Math.pow(2,time)
+            }
+        },
+        // severeCasesByRequestedTime:(data,impact)=>{
 
-    impact.currentlyInfected=data.reportedCases*10;
-    severeImpact.currentlyInfected=data.reportedCases*50;
+        // }
 
-    impact.infectionsByRequestedTime=impact.currentlyInfected * Math.pow(2, 19);
-    severeImpact.infectionsByRequestedTime=severeImpact.currentlyInfected * Math.pow(2,19);
-
-    severeImpact.severeCasesByRequestedTime=severeImpact.infectionsByRequestedTime * 0.15;
-    impact.severeCasesByRequestedTime=impact.infectionsByRequestedTime * 0.15;
-
-    impact.hospitalBedsByRequestedTime=((data.totalHospitalBeds* 0.35)) - impact.severeCasesByRequestedTime
-    severeImpact.hospitalBedsByRequestedTime=((data.totalHospitalBeds* 0.35)) - severeImpact.severeCasesByRequestedTime
-
-    impact.casesForICUByRequestedTime=impact.infectionsByRequestedTime*0.05;
-    severeImpact.casesForICUByRequestedTime=severeImpact.infectionsByRequestedTime*0.05;
-
-    impact.infectionsByRequestedTime=impact.infectionsByRequestedTime*0.2;
-    severeImpact.infectionsByRequestedTime=severeImpact.infectionsByRequestedTime*0.2;
+    
+    }
 
 
 
-    console.log(severeImpact.hospitalBedsByRequestedTime)
-    console.log(impact.hospitalBedsByRequestedTime)
-    console.log(data)
-    console.log(severeImpact.severeCasesByRequestedTime);
-    console.log(impact.severeCasesByRequestedTime);
+
+
+
+
+    // impact.currentlyInfected=(data) => {
+    //     console.log(data.reportedCases * 10)
+    //     return data.reportedCases * 10
+    // }
+    // severeImpact.currentlyInfected=(data)=> data.reportedCases * 50 ; 
+
+
+
+    //  impact.infectionsByRequestedTime=(this) =>{
+    //     console.log(this)
+    //     return impact
+    //  } 
+
+    // impact.currentlyInfected=data.reportedCases*10;
+    // severeImpact.currentlyInfected=data.reportedCases*50;
+
+    // impact.infectionsByRequestedTime=impact.currentlyInfected * Math.pow(2, 19);
+    // severeImpact.infectionsByRequestedTime=severeImpact.currentlyInfected * Math.pow(2,19);
+    // severeImpact.severeCasesByRequestedTime=severeImpact.infectionsByRequestedTime * 0.15;
+    // impact.severeCasesByRequestedTime=impact.infectionsByRequestedTime * 0.15;
+
+    // impact.hospitalBedsByRequestedTime=((data.totalHospitalBeds* 0.35)) - impact.severeCasesByRequestedTime
+    // severeImpact.hospitalBedsByRequestedTime=((data.totalHospitalBeds* 0.35)) - severeImpact.severeCasesByRequestedTime
+
+    // impact.casesForICUByRequestedTime=impact.infectionsByRequestedTime*0.05;
+    // severeImpact.casesForICUByRequestedTime=severeImpact.infectionsByRequestedTime*0.05;
+
+    // impact.infectionsByRequestedTime=impact.infectionsByRequestedTime*0.2;
+    // severeImpact.infectionsByRequestedTime=severeImpact.infectionsByRequestedTime*0.2;
+
+
+
+    // console.log(impact)
+    // console.log(impact.hospitalBedsByRequestedTime)
+    // console.log(data)
+    // console.log(severeImpact.severeCasesByRequestedTime);
+    // console.log(impact.severeCasesByRequestedTime);
 
 };
 
@@ -54,4 +118,5 @@ covid19ImpactEstimator(data);
 
 
 
-export default covid19ImpactEstimator;
+// export default covid19ImpactEstimator;
+    
